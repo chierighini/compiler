0ce764884db7f5e13a2f007a6e7d09e95dede504
chierighini <chierighini.andre@gmail.com>
2021-08-24 19:59:00 -0300

    compilador

--- src/compilador.py
+++ src/compilador.py
@@ -4,6 +4,23 @@ parser = argparse.ArgumentParser(description="Compiler")
 parser.add_argument('script', metavar='S',help='A path to a script to be compiled')
 args = parser.parse_args()
 
-with open (args.script) as f:
-    for line in f:
-        pass
+def main():
+    with open (args.script) as f:
+        content = f.readlines()
+        op_queue = []
+        content_splite = splitter(content)
+
+def splitter(ob):
+    symbol_queue = [x.split(' ') for x in ob]
+    return symbol_queue
+
+def identifier(ob, q):
+    for el in ob:
+        if(isinstance(el, list)):
+            identifier(ob,q)
+        else:
+            pass#logic for identifying elements in the code
+
+
+if __name__ == '__main__':
+    main()
