946dc11fb49d104f364afd0217747cad0e336d7d
gutaolox <32674347+gutaolox@users.noreply.github.com>
2021-10-03 16:04:13 -0300

    Update ConsoleApplication1.cpp

--- Dijkstra Work/ConsoleApplication1/ConsoleApplication1.cpp
+++ Dijkstra Work/ConsoleApplication1/ConsoleApplication1.cpp
@@ -10,79 +10,66 @@
 
 int main()
 {
-	int   G [ MAX ] [ MAX ] , i , j , n , u ;
-	printf( "Enter no. of vertices:" ) ;
-	scanf( "%d" , & n ) ;
-	printf( "\nEnter the adjacency matrix:\n" ) ;
-	for ( i = 0 ; i < n ; i = i + 1 ) {
-		for (j = 0 ; j < n ; j = j + 1) {
-			scanf( "%d" , & G [ i ] [ j ] );
-		}
-	}
-	printf( "\nEnter the starting node:" ) ;
-	scanf( "%d" , & u ) ;
-	
-	int cost[MAX][MAX], distance[MAX], pred[MAX];
-	int visited[MAX], count, mindistance, nextnode;
+	int G [ MAX ] [ MAX ] = { { 0 , 10 , 0 , 30 , 100 } , { 10 , 0 , 50 , 0 , 0 } , { 0 , 50 , 0 , 20 , 10 } , { 30 , 0 , 20 , 0 , 60 } , { 100 , 0 , 10 , 60 , 0 } } , i , j , n = 5 , u = 0 ;
+	int cost [ MAX ] [ MAX ] , distance [ MAX ] , pred [ MAX ] ;
+	int visited [ MAX ] , count , mindistance , nextnode ;
 	//pred[] stores the predecessor of each node
 	//count gives the number of nodes seen so far
 	//create the cost matrix
-	for (i = 0; i < n; i = i + 1) {
-		for (j = 0; j < n; j = j + 1) {
-			if (G[i][j] == 0) {
-				cost[i][j] = INFINITY;
+	for ( i = 0 ; i < n ; i = i + 1 ) {
+		for ( j = 0 ; j < n ; j = j + 1 ) {
+			if (G [ i ] [ j ] == 0) {
+				cost [ i ] [ j ] = INFINITY ;
 			}
 			else {
-				cost[i][j] = G[i][j];
+				cost [ i ] [ j ] = G [ i ] [ j ] ;
 			}
 		}
 	}
 	//initialize pred[],distance[] and visited[]
-	for (i = 0; i < n; i = i + 1)
+	for ( i = 0 ; i < n ; i = i + 1 )
 	{
-		distance[i] = cost[u][i];
-		pred[i] = u;
-		visited[i] = 0;
+		distance [ i ] = cost [ u ] [ i ] ;
+		pred [ i ] = u ;
+		visited [ i ] = 0 ;
 	}
-	distance[u] = 0;
-	visited[u] = 1;
-	count = 1;
-	while (count < n - 1)
+	distance [ u ] = 0 ;
+	visited [ u ] = 1 ;
+	count = 1 ;
+	while ( count < n - 1 )
 	{
 		mindistance = INFINITY;
 		//nextnode gives the node at minimum distance
-		for (i = 0 ; i < n ; i = i + 1) {
-			if (distance[i] < mindistance && !visited[i])
+		for ( i = 0 ; i < n ; i = i + 1 ) {
+			if ( distance [ i ] < mindistance && ! visited [ i ] )
 			{
-				mindistance = distance[i];
-				nextnode = i;
+				mindistance = distance [ i ] ;
+				nextnode = i ;
 			}
 		}
 		//check if a better path exists through nextnode
-		visited[nextnode] = 1;
-		for (i = 0; i < n; i = i + 1) {
-			if (!visited[i]) {
-				if (mindistance + cost[nextnode][i] < distance[i])
+		visited [ nextnode ] = 1 ;
+		for (i = 0 ; i < n ; i = i + 1) {
+			if ( ! visited [ i ] ) {
+				if ( mindistance + cost [ nextnode ] [ i ] < distance [ i ] )
 				{
-					distance[i] = mindistance + cost[nextnode][i];
-					pred[i] = nextnode;
+					distance [ i ] = mindistance + cost [ nextnode ] [ i ] ;
+					pred [ i ] = nextnode ;
 				}
 			}
 		}
-		count = count + 1;
+		count = count + 1 ;
 	}
 
 	//print the path and distance of each node
-	for (i = 0; i < n; i = i + 1) {
+	for (i = 0 ; i < n ; i = i + 1) {
 		if (i != u)
 		{
-			printf("\nDistance of node%d=%d", i, distance[i]);
-			printf("\nPath=%d", i);
-			j = i;
-			while (j != u)
+			j = i ;
+			while ( j != u )
 			{
-				j = pred[j];
-				printf("<-%d", j);
+				j = pred [ j ] ;
+				
 			}
 		}
 	}
