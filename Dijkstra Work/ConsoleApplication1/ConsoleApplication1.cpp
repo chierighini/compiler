f35176ec94b7bff9d2ec753901ac54edbefe57fb
chierighini <chierighini.andre@gmail.com>
2021-10-03 18:37:27 -0300

    entrega 2

--- Dijkstra Work/ConsoleApplication1/ConsoleApplication1.cpp
+++ Dijkstra Work/ConsoleApplication1/ConsoleApplication1.cpp
@@ -10,13 +10,21 @@
 
 int main()
 {
-	int G [ MAX ] [ MAX ] = { { 0 , 10 , 0 , 30 , 100 } , { 10 , 0 , 50 , 0 , 0 } , { 0 , 50 , 0 , 20 , 10 } , { 30 , 0 , 20 , 0 , 60 } , { 100 , 0 , 10 , 60 , 0 } } , i , j , n = 5 , u = 0 ;
+	int G [ MAX ] [ MAX ] = { 
+		{ 0 , 10 , 0 , 30 , 100 } , 
+		{ 10 , 0 , 50 , 0 , 0 } , 
+		{ 0 , 50 , 0 , 20 , 10 } , 
+		{ 30 , 0 , 20 , 0 , 60 } , 
+		{ 100 , 0 , 10 , 60 , 0 } } , 
+		i , j , n = 5 , u = 0 ;
 	int cost [ MAX ] [ MAX ] , distance [ MAX ] , pred [ MAX ] ;
 	int visited [ MAX ] , count , mindistance , nextnode ;
 	//pred[] stores the predecessor of each node
 	//count gives the number of nodes seen so far
 	//create the cost matrix
-	for ( i = 0 ; i < n ; i = i + 1 ) {
+	
+	for (i = 0; i < n; i = i + 1)
+	{
 		for ( j = 0 ; j < n ; j = j + 1 ) {
 			if (G [ i ] [ j ] == 0) {
 				cost [ i ] [ j ] = INFINITY ;
@@ -26,7 +34,7 @@ int main()
 			}
 		}
 	}
-	//initialize pred[],distance[] and visited[]
+
 	for ( i = 0 ; i < n ; i = i + 1 )
 	{
 		distance [ i ] = cost [ u ] [ i ] ;
@@ -39,7 +47,6 @@ int main()
 	while ( count < n - 1 )
 	{
 		mindistance = INFINITY;
-		//nextnode gives the node at minimum distance
 		for ( i = 0 ; i < n ; i = i + 1 ) {
 			if ( distance [ i ] < mindistance && ! visited [ i ] )
 			{
@@ -47,7 +54,6 @@ int main()
 				nextnode = i ;
 			}
 		}
-		//check if a better path exists through nextnode
 		visited [ nextnode ] = 1 ;
 		for (i = 0 ; i < n ; i = i + 1) {
 			if ( ! visited [ i ] ) {
@@ -61,7 +67,6 @@ int main()
 		count = count + 1 ;
 	}
 
-	//print the path and distance of each node
 	for (i = 0 ; i < n ; i = i + 1) {
 		if (i != u)
 		{
@@ -75,13 +80,3 @@ int main()
 	}
 	return 0 ;
 }
-// Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
-// Depurar programa: F5 ou menu Depurar > Iniciar Depuração
-
-// Dicas para Começar: 
-//   1. Use a janela do Gerenciador de Soluções para adicionar/gerenciar arquivos
-//   2. Use a janela do Team Explorer para conectar-se ao controle do código-fonte
-//   3. Use a janela de Saída para ver mensagens de saída do build e outras mensagens
-//   4. Use a janela Lista de Erros para exibir erros
-//   5. Ir Para o Projeto > Adicionar Novo Item para criar novos arquivos de código, ou Projeto > Adicionar Item Existente para adicionar arquivos de código existentes ao projeto
-//   6. No futuro, para abrir este projeto novamente, vá para Arquivo > Abrir > Projeto e selecione o arquivo. sln
--- src/compilador.py
+++ src/compilador.py
@@ -1,19 +1,26 @@
 import os, argparse, json
 
+#argument parser -> usage: python3 compilador.py path_to_script
 parser = argparse.ArgumentParser(description="Compiler")
 parser.add_argument('script', metavar='S',help='A path to a script to be compiled')
 args = parser.parse_args()
 
 def main():
+    '''
+    reads script from first argument path and reserved symbols json file,
+    splits every line per space, calls out identification function and writes out
+    the token list in a temporary file
+    '''
     with open (args.script) as f:
         raw_content = f.readlines()
     with open('src/reserved.json') as res:
         reserved = json.load(res)
     op_queue = []
     content = splitter(raw_content)
-    identifier(content, op_queue,reserved)
-    #print(content)
-    print(op_queue)
+    identifier(content, op_queue,reserved)    
+    with open('tmp/tokens.txt','w') as t:
+        for el in op_queue:
+            t.write(str(el) + '\n')
 
 def splitter(ob):
     '''
@@ -23,19 +30,21 @@ def splitter(ob):
     #final_queue = [x.replace('\n','') for x in symbol_queue]
     return symbol_queue
 
-def identifier(ob, q, reserved):
+def identifier(ob, op_q, reserved):
     '''
     identifies if the elements of a list are not lists, 
-    and recursively gets to the strings for identification
+    and recursively gets to the strings for token identification and 
+    stores them in per-line lists inside a larger list, 
+    removing line breaks and tabs
     '''
+    q= []
     for el in ob:
         if(isinstance(el, list)):
-            identifier(el,q,reserved)
+            identifier(el,op_q,reserved)
         else:
-            if(el.find('\n')!= -1):
-                print('here\n')
+            if(el.find('\n')!= -1 or el.find('\t')!= -1):
                 el = el.replace('\n','')
-                print(el)
+                el = el.replace('\t','')
             if(el in reserved['line_breaker']):
                 q.append('LINE_END')
             elif(el in reserved["words"]):
@@ -58,6 +67,7 @@ def identifier(ob, q, reserved):
                         q.append("VARIABLE")
                 else:
                     q.append("VALUE")
+    op_q.append(q)
 
             
 
